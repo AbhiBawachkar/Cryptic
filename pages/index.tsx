d0d079e07a1af0437b59a41f3743cfb0f3b869d2
AbhiBawachkar <bawachkar.abhi2000@gmail.com>
2022-08-20 22:02:08 +0530

    added coin swap feature

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Header from '../components/header'
 import Trending from '../components/trending'
 import CMCTable from '../components/cmc-table/CMCTable'
+import SwapCryptoModal from '../components/swapCryptoModal'
 
 export default function Home() {
   return (
     <div className='min-h-screen'>
       <Header />
+      <SwapCryptoModal />
       <div className='mt-10' />
       <Trending />
       <div className='mt-20' />
